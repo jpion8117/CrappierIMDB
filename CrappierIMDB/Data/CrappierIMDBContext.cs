9888b9190ad62661567f8db060d96db7bab129d3
jp8117 <jp8117@live.com>
2023-08-31 18:15:23 -0400

    Add project files.

---
+++ CrappierIMDB/Data/CrappierIMDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CrappierIMDB.Models;
+
+namespace CrappierIMDB.Data
+{
+    public class CrappierIMDBContext : DbContext
+    {
+        public CrappierIMDBContext (DbContextOptions<CrappierIMDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CrappierIMDB.Models.Movie> Movie { get; set; } = default!;
+    }
+}
